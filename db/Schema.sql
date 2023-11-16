93aece1ac14bf16122d98af0e2026ac26a630e09
Róger Roca Arano <rogerroca@reinseg.com>
2023-11-16 14:03:09 -0400

    Se adapta script de la base de datos para usar con EF.

--- db/Schema.sql
+++ db/Schema.sql
@@ -13,31 +13,37 @@ $$;
 
 -- Usuarios y Roles del sistema Web
 
-create table "Roles"(
-    "id" serial primary key not null,
-    "rol" varchar(32) not null
-);
-insert into "Roles"("rol") VALUES ('ADMINISTRADOR'),
-                                  ('CLIENTE'),
-                                  ('RECEPCION'),
-                                  ('RESERVA'),
-                                  ('INVENTARIO'),
-                                  ('CAJA');
-create table "Usuarios"(
-    "id" serial primary key not null,
-    "usuario" varchar(64) not null,
-    "hashPassword" varchar(512) not null,
-    "salHash" varchar(64) not null,
-    "habilitado" boolean default true
-);
-create table "RolUsuario"(
-    "idUsuario" serial references "Usuarios"(id) not null,
-    "idRol" serial references "Roles"(id) not null
-);
+-- create table "Roles"(
+--     "id" serial primary key not null,
+--     "rol" varchar(32) not null
+-- );
+-- insert into "Roles"("rol") VALUES ('ADMINISTRADOR'),
+--                                   ('CLIENTE'),
+--                                   ('RECEPCION'),
+--                                   ('RESERVA'),
+--                                   ('INVENTARIO'),
+--                                   ('CAJA');
+-- create table "Usuarios"(
+--     "id" serial primary key not null,
+--     "usuario" varchar(64) not null,
+--     "hashPassword" varchar(512) not null,
+--     "salHash" varchar(64) not null,
+--     "habilitado" boolean default true
+-- );
+-- create table "RolUsuario"(
+--     "idUsuario" serial references "Usuarios"(id) not null,
+--     "idRol" serial references "Roles"(id) not null
+-- );
+
+-- 2023-11-16: Manejaremos la autenticación mediante ASP.Net Core Identity.
+-- Referencias:
+-- https://learn.microsoft.com/en-us/aspnet/core/security/authentication/identity?view=aspnetcore-7.0&tabs=visual-studio
+-- https://www.c-sharpcorner.com/article/how-to-add-authentication-in-asp-net-core-6-mvc-project-using-identity/
+
 
 -- Entidades principales del negocio
 
-create table "Clientes"(
+create table "Cliente"(
     "id" serial primary key not null,
     "nombres" varchar(128) not null,
     "apellidos" varchar(128) not null,
@@ -45,7 +51,7 @@ create table "Clientes"(
     "nroRazonSocial" varchar(64),
     "email" varchar(128)
 );
-create table "Huespedes"(
+create table "Huesped"(
     "id" serial primary key not null,
     "nombres" varchar(128) not null,
     "apellidos" varchar(128) not null,
@@ -53,21 +59,21 @@ create table "Huespedes"(
     "tipoDocIdentidad" varchar(16) not null,
     "nacionalidad" varchar(16) not null
 );
-create table "TipoHabitaciones"(
+create table "TipoHabitacion"(
     "id" serial primary key not null,
     "nombre" varchar(128) not null,
     "descripcion" varchar(256),
     "huespedesPermitidos" int not null,
     "precioNoche" float not null
 );
-create table "Habitaciones"(
+create table "Habitacion"(
     "id" serial primary key not null,
-    "idTipoHabitacion" serial references "TipoHabitaciones"(id) not null,
+    "idTipoHabitacion" serial references "TipoHabitacion"(id) not null,
     "habilitado" bool default true,
     "reservado" bool default false,
     "nro" varchar(16) not null
 );
-create table "PaquetesPromocionales"(
+create table "PaquetePromocional"(
     "id" serial primary key not null,
     "nombre" varchar(128) not null,
     "descripcion" varchar(256) not null,
@@ -76,8 +82,10 @@ create table "PaquetesPromocionales"(
     "fechaDisponibleInicio" timestamp not null,
     "fechaDisponibleFin" timestamp not null
 );
-create table "Reservas"(
+create table "Reserva"(
     "id" serial primary key not null,
+    "idCliente" serial references "Cliente"(id),
+    "idHabitacion" serial references "Habitacion"(id),
     "fechaReserva" timestamp default now(),
     "inicioReserva" timestamp,
     "finReserva" timestamp,
@@ -86,54 +94,54 @@ create table "Reservas"(
 
 -- Reportes Reservas
 
-create table "ReservasClientes"(
-    "idReserva" serial references "Reservas"(id),
-    "idCliente" serial references "Clientes"(id)
-);
-create table "HabitacionesReservadas"(
-    "idHabitacion" serial references "Habitaciones"(id),
-    "idReserva" serial references "Reservas"(id)
-);
-create table "PaquetesPromocionalesReservas"(
-    "idPaquetePromocional" serial references "PaquetesPromocionales"(id),
-    "idReserva" serial references "Reservas"(id)
-);
+-- create table "ReservaCliente"(
+--     "idReserva" serial references "Reserva"(id),
+--     "idCliente" serial references "Cliente"(id)
+-- );
+-- create table "HabitacionReservada"(
+--     "idHabitacion" serial references "Habitacion"(id),
+--     "idReserva" serial references "Reserva"(id)
+-- );
+-- create table "PaquetePromocionalReserva"(
+--     "idPaquetePromocional" serial references "PaquetePromocional"(id),
+--     "idReserva" serial references "Reserva"(id)
+-- );
 
 -- Registro de ingreso y salida de huéspedes
 
 create table "CheckIn"(
-    "idHuesped" serial references "Huespedes"(id) not null,
-    "idHabitacion" serial references "Habitaciones"(id) not null,
+    "idHuesped" serial references "Huesped"(id) not null,
+    "idHabitacion" serial references "Habitacion"(id) not null,
     "ingreso" timestamp default now()
 );
 create table "CheckOut"(
-    "idHuesped" serial references "Huespedes"(id) not null,
-    "idHabitacion" serial references "Habitaciones"(id) not null,
+    "idHuesped" serial references "Huesped"(id) not null,
+    "idHabitacion" serial references "Habitacion"(id) not null,
     "salida" timestamp default now()
 );
 
 -- Inventario Habitación
 
-create table "ItemsHabitacion"(
+create table "ItemHabitacion"(
     "id" serial primary key not null,
     "descripcion" varchar(128) not null,
     "costo" float not null
 );
-create table "ItemsIncluidosHabitacion"(
-    "idHabitacion" serial references "Habitaciones"(id),
-    "idItem" serial references "ItemsHabitacion"(id)
+create table "InventarioHabitacion"(
+    "idHabitacion" serial references "Habitacion"(id),
+    "idItem" serial references "ItemHabitacion"(id)
 );
 create table "InventarioReposicion"(
     "id" serial primary key not null,
-    "idHabitacion" serial references "Habitaciones"(id),
-    "idItemHabitacion" serial references "ItemsHabitacion"(id)
+    "idHabitacion" serial references "Habitacion"(id),
+    "idItemHabitacion" serial references "ItemHabitacion"(id)
 );
 
 -- Cobro
 
-create table "Cobros"(
+create table "Cobro"(
     "id" serial primary key not null,
-    "idReserva" serial references "Reservas"(id) not null,
+    "idReserva" serial references "Reserva"(id) not null,
     "descripcion" varchar(128),
     "total" float not null,
     "estado" varchar(64)
@@ -141,24 +149,24 @@ create table "Cobros"(
 
 -- Pagos
 
-create table "Pagos"(
+create table "Pago"(
     "id" serial primary key not null,
-    "idCobro" serial references "Cobros"(id) not null,
+    "idCobro" serial references "Cobro"(id) not null,
     "monto" float not null
 );
-create table "PagosQr"(
-    "idPago" serial references "Pagos"(id) not null,
+create table "PagoQr"(
+    "idPago" serial references "Pago"(id) not null,
     "nroTransaccion" varchar(128) not null
 );
-create table "PagosPos"(
-    "idPago" serial references "Pagos"(id) not null,
+create table "PagoTarjeta"(
+    "idPago" serial references "Pago"(id) not null,
     "tipoTarjeta" varchar(16) not null,
     "ultimosDigTarjeta" int not null,
     "autorizacion" varchar(32) not null,
     "operacion" varchar(32) not null
 );
-create table if not exists "PagosStripe"(
-    "idPago" serial references "Pagos"(id) not null,
+create table if not exists "PagoStripe"(
+    "idPago" serial references "Pago"(id) not null,
     "idStripe" varchar(64) not null,
     "estado" varchar(32) not null
 );
