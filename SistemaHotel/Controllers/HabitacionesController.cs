a69e937774c124809350d21b839ec0d233ef60c0
Róger Roca Arano <rogerroca@reinseg.com>
2023-12-05 22:10:13 -0400

    Remove guest related views and controller
    Guest related views and controller were removed since they are unnecessary for the current application flow. These include "create", "delete", "edit" and "details" views for guests and GuestController. This change helps to clean up the codebase and ensure that only necessary code is maintained.

--- SistemaHotel/Controllers/HabitacionesController.cs
+++ SistemaHotel/Controllers/HabitacionesController.cs
@@ -1,13 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SistemaHotel.Data;
 using SistemaHotel.Models;
 
 namespace SistemaHotel.Controllers;
 
-[Authorize (Roles = "ADMINISTRADOR")]
+[Authorize (Roles = "EMPLEADO")]
 public class HabitacionesController : Controller
 {
     private readonly Database _context;
@@ -17,133 +20,146 @@ public class HabitacionesController : Controller
         _context = context;
     }
 
+    private async Task<List<TipoHabitacion>> GetTiposHabitacion()
+    {
+        return await _context.TipoHabitacion.ToListAsync();
+    }
+
     // GET: Habitaciones
     public async Task<IActionResult> Index()
     {
-        var database = _context.Habitacion.Include(h => h.IdTipoHabitacionNavigation);
-        return View(await database.ToListAsync());
+        var habitaciones = await _context.Habitacion
+            .OrderBy(habitacion => habitacion.Id)
+            .ToListAsync();
+        ViewBag.TiposHabitacion = await GetTiposHabitacion();
+        return View(habitaciones);
     }
-
-    // GET: Habitaciones/Details/5
-    public async Task<IActionResult> Details(int? id)
-    {
-        if (id == null || _context.Habitacion == null) return NotFound();
-
-        var habitacion = await _context.Habitacion
-            .Include(h => h.IdTipoHabitacionNavigation)
-            .FirstOrDefaultAsync(m => m.Id == id);
-        if (habitacion == null) return NotFound();
-
-        return View(habitacion);
-    }
-
     // GET: Habitaciones/Create
-    public IActionResult Create()
+    public async Task<IActionResult> Create()
     {
-        ViewData["IdTipoHabitacion"] = new SelectList(_context.TipoHabitacion, "Id", "Nombre");
+        ViewBag.TiposHabitacion = await GetTiposHabitacion();
         return View();
     }
 
     // POST: Habitaciones/Create
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
     [HttpPost]
     [ValidateAntiForgeryToken]
     public async Task<IActionResult> Create(
-        [Bind("Id,IdTipoHabitacion,Habilitado,Reservado,Nro")]
+        [Bind("Id," +
+              "IdTipoHabitacion," +
+              "Piso," +
+              "Ubicacion")]
         Habitacion habitacion)
     {
-        // Cargar el TipoHabitacion asociado al IdTipoHabitacion desde la base de datos
-        // var tipoHabitacion = _context.TipoHabitacion.Find(habitacion.IdTipoHabitacion);
-        // habitacion.IdTipoHabitacionNavigation = tipoHabitacion;
-        if (ModelState.IsValid)
+        habitacion.Habilitado = true;
+        habitacion.Disponible = true;
+        habitacion.IdTipoHabitacionNavigation = await _context.TipoHabitacion.FindAsync(habitacion.IdTipoHabitacion);
+        // Verificar si existe habitación con el mismo id
+        if (_context.Habitacion.Any(h => h.Id == habitacion.Id))
         {
-            _context.Add(habitacion);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            Console.WriteLine("Ya existe una habitación con el mismo id.");
+            ModelState.AddModelError("Id", "Ya existe una habitación con el mismo id.");
         }
-
-        var errors = ModelState.Values.SelectMany(v => v.Errors);
-        ViewData["IdTipoHabitacion"] =
-            new SelectList(_context.TipoHabitacion, "Id", "Nombre", habitacion.IdTipoHabitacion);
-        return View(habitacion);
-    }
-
-    // GET: Habitaciones/Edit/5
-    public async Task<IActionResult> Edit(int? id)
-    {
-        if (id == null || _context.Habitacion == null) return NotFound();
-
-        var habitacion = await _context.Habitacion.FindAsync(id);
-        if (habitacion == null) return NotFound();
-        ViewData["IdTipoHabitacion"] = new SelectList(_context.TipoHabitacion, "Id", "Nombre" +
-            "" +
-            "", habitacion.IdTipoHabitacion);
-        return View(habitacion);
-    }
-
-    // POST: Habitaciones/Edit/5
-    // To protect from overposting attacks, enable the specific properties you want to bind to.
-    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-    [HttpPost]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Edit(int id,
-        [Bind("Id,Nro,IdTipoHabitacion,Habilitado,Reservado")]
-        Habitacion habitacion)
-    {
-        if (id != habitacion.Id) return NotFound();
-
-        if (ModelState.IsValid)
+        // Verificar si existen errores en el modelo
+        if (!ModelState.IsValid)
         {
-            try
+            Console.WriteLine("Existen errores en el modelo.");
+            foreach (var modelStateKey in ModelState.Keys)
             {
-                _context.Update(habitacion);
-                await _context.SaveChangesAsync();
+                var modelStateVal = ModelState[modelStateKey];
+                foreach (var error in modelStateVal.Errors)
+                {
+                    Console.WriteLine($"Key: {modelStateKey}, Error: {error.ErrorMessage}");
+                }
             }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!HabitacionExists(habitacion.Id))
-                    return NotFound();
-                throw;
-            }
-
-            return RedirectToAction(nameof(Index));
+            ViewBag.TiposHabitacion = await GetTiposHabitacion();
+            return View(habitacion);
         }
-
-        var errors = ModelState.Values.SelectMany(v => v.Errors);
-        ViewData["IdTipoHabitacion"] = new SelectList(_context.TipoHabitacion, "Id", "Id", habitacion.IdTipoHabitacion);
-        return View(habitacion);
-    }
-
-    // GET: Habitaciones/Delete/5
-    public async Task<IActionResult> Delete(int? id)
-    {
-        if (id == null || _context.Habitacion == null) return NotFound();
-
-        var habitacion = await _context.Habitacion
-            .Include(h => h.IdTipoHabitacionNavigation)
-            .FirstOrDefaultAsync(m => m.Id == id);
-        if (habitacion == null) return NotFound();
-
-        return View(habitacion);
-    }
-
-    // POST: Habitaciones/Delete/5
-    [HttpPost]
-    [ActionName("Delete")]
-    [ValidateAntiForgeryToken]
-    public async Task<IActionResult> DeleteConfirmed(int id)
-    {
-        if (_context.Habitacion == null) return Problem("Entity set 'Database.Habitacion'  is null.");
-        var habitacion = await _context.Habitacion.FindAsync(id);
-        if (habitacion != null) _context.Habitacion.Remove(habitacion);
-
+        // Agregar la habitación a la base de datos
+        _context.Add(habitacion);
         await _context.SaveChangesAsync();
+        // Redireccionar a la lista de habitaciones
         return RedirectToAction(nameof(Index));
     }
 
-    private bool HabitacionExists(int id)
+    // GET: Habitaciones/Edit/Id
+    public async Task<IActionResult> Edit(int? id)
     {
-        return (_context.Habitacion?.Any(e => e.Id == id)).GetValueOrDefault();
+        if (id == null)
+        {
+            return NotFound();
+        }
+        var habitacion = await _context.Habitacion.FindAsync(id);
+        if (habitacion == null)
+        {
+            return NotFound();
+        }
+        ViewBag.TiposHabitacion = await GetTiposHabitacion();
+        return View(habitacion);
+    }
+
+    // POST: Habitaciones/Edit/Id
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(
+        int id,
+        [Bind("Id," +
+              "IdTipoHabitacion," +
+              "Piso," +
+              "Ubicacion," +
+              "Habilitado")]
+        Habitacion habitacion)
+    {
+        if (id != habitacion.Id)
+        {
+            return NotFound();
+        }
+        // Verificar si existen errores en el modelo
+        if (!ModelState.IsValid)
+        {
+            Console.WriteLine("Existen errores en el modelo.");
+            foreach (var modelStateKey in ModelState.Keys)
+            {
+                var modelStateVal = ModelState[modelStateKey];
+                foreach (var error in modelStateVal.Errors)
+                {
+                    Console.WriteLine($"Key: {modelStateKey}, Error: {error.ErrorMessage}");
+                }
+            }
+            ViewBag.TiposHabitacion = await GetTiposHabitacion();
+            return View(habitacion);
+        }
+        // Actualizar la habitación en la base de datos
+        _context.Update(habitacion);
+        await _context.SaveChangesAsync();
+        // Redireccionar a la lista de habitaciones
+        return RedirectToAction(nameof(Index));
+    }
+    
+    // GET: Habitaciones/Delete/Id
+    public async Task<IActionResult> Delete(int? id)
+    {
+        if (id == null)
+        {
+            return NotFound();
+        }
+        var habitacion = await _context.Habitacion.FindAsync(id);
+        if (habitacion == null)
+        {
+            return NotFound();
+        }
+        ViewBag.TiposHabitacion = await GetTiposHabitacion();
+        return View(habitacion);
+    }
+
+    // POST: Habitaciones/Delete/Id
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id)
+    {
+        var habitacion = await _context.Habitacion.FindAsync(id);
+        _context.Habitacion.Remove(habitacion);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
     }
 }
